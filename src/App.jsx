dca4e52c5c6a2e145788b9dcc8096eece1b24c5d
Joseph Wilfong <josephwilfong91@gmail.com>
2025-05-22 12:10:59 -0700

    grammar

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,7 @@ function App() {
         <div className="card border-0 shadow-sm p-4 mx-auto" style={{ maxWidth: 600 }}>
         <h2 className="mb-3">About Me</h2>
         <p>
-            Hey, I’m Joseph. I’m a software developer who swapped tools and job sites for code editors and web apps. When I’m not building or fixing something in React or Python, I’m probably outside—hiking mossy forests in Washington, discovering hidden trails, or just enjoying some time unplugged in the mountains.
+            Hey, I’m Joseph! I’m a software developer who swapped tools and job sites for code editors and web apps. When I’m not building or fixing something in React or Python, I’m probably outside—hiking mossy forests in Washington, discovering hidden trails, or just enjoying some time unplugged in the mountains.
             <br /><br />
             I love solving real-world problems and making tech more approachable. My favorite projects are the ones that combine practical needs with clean design, and I enjoy learning by getting my hands dirty—whether that’s digging into a new JavaScript library or an overgrown trail.
             <br /><br />
